af4d3fd3b733a1f99955b038d3efecfceb7c43e9
Ivan Zoccolan <ivan.zoccolan@gmail.com>
2021-06-22 17:28:04 +0200

    First release

---
+++ processes.py
@@ -0,0 +1,126 @@
+from abc import abstractmethod
+from abc import ABCMeta
+import numpy as np
+from scipy.integrate import quad
+from scipy.special import gamma
+
+
+class BaseProcess(metaclass=ABCMeta):
+    """ Base class for Levy stochastic processes {X(t): t >= 0}
+        defined by their characteristic function.
+    """
+    @abstractmethod
+    def __init__(self): pass
+
+    @abstractmethod
+    def characteristic(self, u, time): pass
+
+    def density(self, x, time=1):
+        """
+        Probability density function of the X(t) variable of the process.
+
+        Parameters
+        ----------
+        x: (float) x-coordinate.
+        time: (float) time coordinate.
+
+        Returns
+        -------
+        float: value of the pdf of X(t) at point x f(x, t).
+
+        Notes
+        -----
+        The pdf is numerically calculated from the characteristic function
+        by means of the Pelaez formula.
+        """
+        return 1 / np.pi * quad(lambda u: np.real(np.exp(-u * x * 1j) * self.characteristic(u, time)),
+                                1e-15,
+                                np.inf)[0]
+
+
+class VGProcess(BaseProcess):
+    """
+    Class for the Variance Gamma stochastic process.
+
+    Parameters
+    ----------
+    mu: (float) drift of the Brownian motion.
+    sigma: (float) volatility of the Brownian motion.
+    nu: (float) variance of the Gamma process
+    """
+    def __init__(self, mu=-0.3150, sigma=0.1301, nu=0.1753):
+        super().__init__()
+        self._mu = mu
+        if sigma > 0.:
+            self._sigma = sigma
+        else:
+            raise KeyError("sigma must be strictly greater than zero.")
+        if nu > 0.:
+            self._nu = nu
+        else:
+            raise KeyError("nu must be strictly greater than zero.")
+
+    def characteristic(self, u, time=1):
+        """
+        Characteristic function Phi(u) of the X(t) variable of the Variance Gamma process.
+
+        Parameters
+        ----------
+        u: (float) u-coordinate of the characteristic function.
+        time: (float) time t coordinate of the process {X(t) t>=0}.
+
+        Returns
+        ------
+        float: Phi(u) ordinate value mapping the u-coordinate.
+        """
+        const = -(time/self._nu)
+        esp = np.log(1 - 1j * u * self._mu * self._nu + 0.5 * self._sigma ** 2 * u ** 2 * self._nu)
+        return np.exp(const * esp)
+
+
+class CGMYProcess(BaseProcess):
+    """
+    Class for the Carr, Geman, Madan, Yor (CGMY) stochastic process.
+
+    Parameters
+    ----------
+    g: (float) right skewness parameter.
+    m: (float) left skewness parameter.
+    y: (float) parameter describing the Levy density near 0.
+    c: (float) parameter measure of the overall level of activity.
+    """
+    def __init__(self, c=0.6817, g=18.0293, m=57.6750, y=0.8):
+        super().__init__()
+        if g >= 0.:
+            self._g = g
+        else:
+            raise KeyError("g must be greater of equal zero.")
+        if m >= 0.:
+            self._m = m
+        else:
+            raise KeyError("m must be greater of equal zero.")
+        if c > 0.:
+            self._c = c
+        else:
+            raise KeyError("c must be strictly greater than zero.")
+        if y < 2.:
+            self._y = y
+        else:
+            raise KeyError("y must be strictly less than 2.")
+
+    def characteristic(self, u, time=1):
+        """
+        Characteristic function Phi(u) of the X(t) variable of the CGMY process.
+
+        Parameters
+        ----------
+        u: (float) u-coordinate of the characteristic function.
+        time: (float) time t coordinate of the process {X(t) t>=0}.
+
+        Returns
+        ------
+        float: Phi(u) ordinate value mapping the u-coordinate.
+        """
+        left = (self._m - 1j*u)**self._y - self._m**self._y
+        right = (self._g + 1j*u)**self._y - self._g**self._y
+        return np.exp(self._c * time * gamma(-self._y) * (left + right))
---
+++ venv/gmwb.py
@@ -0,0 +1,113 @@
+
+import numpy as np
+from scipy import interpolate
+from scipy.integrate import trapz
+
+
+class Contract:
+    """
+
+    """
+    def __init__(self, process,  **kwargs):
+        # User defined data members
+        self.process = process
+        self.maturity = 20
+        self.fee = 0.0046
+        self.spot = 0.03
+        self.premium = 100
+        self.g_amount = 100
+        self.withdraw_amount = 5
+        self.div = 0.0
+        self.penalty = 0.05
+        # Steps to discretize time, guaranteed account and personal account
+        self.t_step = 1  # time step
+        self.g_step = 10  # guaranteed account step
+        self.p_step = 10  # personal account step
+        # Integration limits
+        self.right_limit = -3.0
+        self.left_limit = 3.0
+
+        for k, v in kwargs.items():
+            if k in self.__dict__:
+                setattr(self, k, v)
+            else:
+                raise KeyError(k)
+
+        # Internal data members
+        # Max values for the accounts
+        self._max_g_account = self.premium  # max guaranteed account
+        self._k = 5  # internal factor to calculate the max personal account
+        self._max_p_account = self._k * self._max_g_account  # max personal account
+        # Variables for the pricing algorithm
+        self._H = int(self._max_g_account / self.g_step) + 1
+        self._L = int(self._max_p_account / self.p_step) + 1
+        self._N = int(self.maturity / self.t_step) + 1
+        self._g_account = np.linspace(0, self._max_g_account, self._H)
+        self._p_account = np.linspace(0, self._max_p_account, self._L)
+        # constants for the integration (step 2. II)
+        self._d = np.real(-np.log(self.process.characteristic(-1j)))
+        self._beta = np.exp((self.spot - self.div + self._d)*self.t_step) * (1 - self.fee*self.t_step)
+        self._m = np.exp(-self.spot*self.t_step)
+        self._xx = np.linspace(self.left_limit, self.right_limit, 1000)
+        self._yy = [self._beta * np.exp(x) for x in self._xx]
+        self._density = [self.process.density(x) for x in self._xx]
+        self._values = []  # list to gather all calculation steps.
+        self._splines = []
+        self._price = 0.0
+
+    def set_fee(self, fee=0.0046):
+        self.fee = fee
+        self._beta = np.exp((self.spot - self.div + self._d) * self.t_step) * (1 - self.fee * self.t_step)
+        self._yy = [self._beta * np.exp(x) for x in self._xx]
+
+    def set_spot(self, spot=0.03):
+        self.spot = spot
+        self._beta = np.exp((self.spot - self.div + self._d) * self.t_step) * (1 - self.fee * self.t_step)
+        self._m = np.exp(-self.spot * self.t_step)
+        self._yy = [self._beta * np.exp(x) for x in self._xx]
+
+    def set_gstep(self, gstep=1):
+        self.g_step = gstep
+        self._H = int(self._max_g_account / self.g_step) + 1
+        self._g_account = np.linspace(0, self._max_g_account, self._H)
+
+    def set_pstep(self, pstep=1):
+        self.p_step = pstep
+        self._L = int(self._max_p_account / self.p_step) + 1
+        self._p_account = np.linspace(0, self._max_p_account, self._L)
+
+    def price(self):
+        # Initial contract value
+        gg, pp = np.meshgrid(self._g_account, self._p_account)
+        self._values.append(np.maximum(gg, pp))
+
+        # Evaluate the contract value at each point of the grid
+        @np.vectorize
+        def value(a, w, ff, theta):
+            cc = theta - self.penalty*max(theta - min(self.withdraw_amount, a), 0)
+            if theta <= min(self.withdraw_amount, a):
+                aa = a - theta
+            else:
+                if w != 0:
+                    aa = max(min(a - theta, a*(1-theta/w)), 0)
+                else:
+                    aa = 0
+
+            integrand = [ff(max(w - theta, 0) * y, aa).item() * dens for y, dens in zip(self._yy, self._density)]
+            return cc + self._m * trapz(integrand, self._xx)
+
+        # Static approach
+        theta_val = self.withdraw_amount
+        for t in np.arange(self._N - 2, 0, -self.t_step):
+            # Step 2. I Interpolate the H x L triplets
+            val_func = interpolate.RectBivariateSpline(self._p_account, self._g_account,  self._values[-1])
+            self._splines.append(val_func)
+            # Step 2. II Compute the contract value at each time step.
+            self._values.append(value(gg, pp, val_func, theta_val))
+
+        # Contract value at inception
+        val_func = interpolate.RectBivariateSpline(self._p_account, self._g_account, self._values[-1])
+        self._splines.append(val_func)
+        final_integrand = [val_func(self.premium*y, self.premium).item() * dens for y, dens in zip(self._yy, self._density)]
+        self._price = self._m * trapz(final_integrand, self._xx)
+        return self._price
